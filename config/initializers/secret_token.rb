cd10419d82dce89a5cc00cf75a4c35c03511177a
mikeaddison93 <mikeaddison93@gmail.com>
2011-04-16 12:51:04 -0400

    post-bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '05cb3aa93d5305f7d4b56dda6984b91db52c87d3bbcaec4f766e09da00a27ebc3871a724448f8212e47d66a1724fb6169654dcaddbe82724d9d53d8fb80acdf7'
